bbfe66ec6810e56a2a14a50dc69f10cc7905f885
davidecaputo <daca9621@gmail.com>
2023-03-29 00:49:39 +0200

    Add image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            title: 'Ciao. Coma và?'
+            title: 'Ciao. Coma và?',
+            image: './img/Image.jpg'
         }
     }
 }).mount('#app');
